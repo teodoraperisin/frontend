1c281ac33d656391261bee88790e25316495c622
teodo <teodo@DESKTOP-QC5H3OF>
2021-03-10 20:31:05 +0100

    vezbe2

--- Starter-project/src/main/java/rva/StarterProjectApplication.java
+++ Starter-project/src/main/java/rva/StarterProjectApplication.java
@@ -1,11 +1,11 @@
-package rva;
+	package rva;//rutni folder
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // GitHub Classroom Invite Link --> https://classroom.github.com/a/PgzNRc2E
 
-@SpringBootApplication
+@SpringBootApplication//konfiguraciona anotacija koja oznacava klasu koja ce definisati spring binove
 public class StarterProjectApplication {
 
 	public static void main(String[] args) {
