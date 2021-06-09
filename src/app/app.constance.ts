dbf7a2b63bd080275c5e20fb1bc9c25d57e1e6f2
teodoraperisin <teodora.perisin@uns.ac.rs>
2021-06-09 15:04:57 +0200

    promena endpointa

--- src/app/app.constance.ts
+++ src/app/app.constance.ts
@@ -1,5 +1,15 @@
-export const PREDUZECE_URL= 'http://localhost:8083/preduzece';
-export const SEKTOR_URL= 'http://localhost:8083/sektor';
-export const RADNIKID_URL= 'http://localhost:8083/radnikBySektor';
-export const RADNIK_URL= 'http://localhost:8083/radnik';
-export const OBRAZOVANJE_URL= 'http://localhost:8083/obrazovanje';
+export const PREDUZECE_URL= 'https://it10g2018-rva.herokuapp.com/preduzece';
+export const SEKTOR_URL= 'https://it10g2018-rva.herokuapp.com/sektor';
+export const RADNIKID_URL= 'https://it10g2018-rva.herokuapp.com/radnikBySektor';
+export const RADNIK_URL= 'https://it10g2018-rva.herokuapp.com/radnik';
+export  const OBRAZOVANJE_URL= 'https://it10g2018-rva.herokuapp.com/obrazovanje';
+
+
+
+//export const PREDUZECE_URL= 'http://localhost:8083/preduzece';
+//export const SEKTOR_URL= 'http://localhost:8083/sektor';
+//export const RADNIKID_URL= 'http://localhost:8083/radnikBySektor';
+//export const RADNIK_URL= 'http://localhost:8083/radnik';
+//export  const OBRAZOVANJE_URL= 'http://localhost:8083/obrazovanje';
+
+
